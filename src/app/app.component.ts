97c29f9b81770f7e858a909cfa8cc14f65563c51
javaExactus <romulo.neves@exactus.com.br>
2025-05-19 20:57:07 -0300

    projeto final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
-import { RodapeComponent } from "./componentes/rodape/rodape.component";
-import { LivroComponent } from "./componentes/livro/livro.component";
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { ListaLivrosComponent } from './componentes/lista-livros/lista-livros.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CabecalhoComponent, RodapeComponent, LivroComponent],
+  imports: [
+    CabecalhoComponent,
+    RodapeComponent,
+    ListaLivrosComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'cursoorgano';
+  title = 'organo';
 }
